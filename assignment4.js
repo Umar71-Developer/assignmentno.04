d49727d80f442fc8976b48b6d4473dde6363446f
Umar71-Developer <umarhayyat710000@gmail.com>
2023-08-28 23:36:15 +0500

    I have created assignment no 4

--- assignment4.js
+++ assignment4.js
@@ -6,13 +6,14 @@
 //  - Develop a program that reads a list of grades and uses the splice method to remove failing grades (below 50) from the array.
 //  - Write a program that uses a function to find the largest element in an array of numbers.
 //Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
-// var n:number = 100;
-// var sum:number = 0;
-// for (let i = 0; i <n; i++) {
-//  if (i%2 == 0) {
-//     sum+=i;
-//  }}
-//  console.log(sum);
+var n = 100;
+var sum = 0;
+for (let i = 0; i < n; i++) {
+    if (i % 2 == 0) {
+        sum += i;
+    }
+}
+console.log(sum);
 //Implement a program that uses a for loop to iterate through an array of numbers and print only the even numbers.
 // var array:number[] = [2,4,5,6,7,8,9,12,35,68,45,78,33,45,66,];
 // for (let i = 0; i < array.length; i++) {
@@ -32,11 +33,11 @@
 // }
 // console.log(area(20));
 //Develop a program that reads a list of grades and uses the splice method to remove failing grades (below 50) from the array.
-var grades = [10, 20, 30, 40, 60, 70, 80, 90];
-for (let i = 0; i <= grades.length; i++) {
-    if (grades[i] <= 50) {
-        grades.splice(i, 1);
-        i--;
-    }
-}
-console.log(grades);
+// var grades:number[] = [10,20,30,40,60,70,80,90];
+// for (let i = 0; i <= grades.length; i++) {
+//     if (grades[i]<=50) {
+//         grades.splice(i,1)
+//         i--;
+//     }}
+//     console.log(grades);
+//Write a program that uses a function to find the largest element in an array of numbers.
